79d4223f5ad22d02e966a5b771a588efc1c813bd
Suyama Daichi <symdit@gmail.com>
2024-03-31 03:33:26 +0900

    setting: ESLintルールを追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,34 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     semi: ['error', 'always'],
+    indent: ['error', 2],
+    quotes: ["error", "single"],
+    "@typescript-eslint/no-unused-vars": "off", // unused-imports/no-unused-importsとの干渉防止
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+        "warn",
+        {
+            "vars": "all",
+            "varsIgnorePattern": "^_",
+            "args": "after-used",
+            "argsIgnorePattern": "^_",
+        },
+    ],
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'keyword-spacing': ["error", { "before" : true }],
+    'comma-spacing': ["error", { "before": false, "after": true }],
+    'array-bracket-spacing': ["error", "never"],
+    'object-curly-spacing': ["error", "always"],
+    'space-in-parens': ["error", "never"],
+    'key-spacing': ["error", { "beforeColon": false }],
+    // ESLintによる自動修正不可のルール
+    'max-len': ['error', { code: 200 }],
   },
 }
